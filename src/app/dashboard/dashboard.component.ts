7177d199bcdd6f10807af7bf3695e0b4f15cf26e
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2021-09-28 15:21:39 -0500

    Implement Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  private daysArray = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+  private monthArray = ['Diciembre', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre'];
+
+  private date = new Date();
+
+  public hour: any;
+  public minute: string | undefined;
+  public second: string | undefined;
+  public ampm: string | undefined;
+
+  public day: string | undefined;
+  public numDay: number | undefined;
+  public month: string | undefined;
+  public year: number | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      const date = new Date();
+      this.updateDate(date);
+    }, 1000);
+
+    this.day = this.daysArray[this.date.getDay()];
+    this.numDay = this.date.getUTCDate();
+    this.month = this.monthArray[this.date.getMonth()];
+    this.year = this.date.getFullYear();
   }
 
+  private updateDate(date: Date) {
+    const hours = date.getHours();
+
+    this.ampm = hours >= 12 ? 'PM' : 'AM';
+
+    this.hour = this.hour ? this.hour : 12;
+    this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+
+    const minutes = date.getMinutes();
+    this.minute = minutes < 10 ? '0' + minutes : minutes.toString();
+
+    const seconds = date.getSeconds();
+    this.second = seconds < 10 ? '0' + seconds : seconds.toString();
+  }
 }
